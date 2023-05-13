347c5b6c97177be7417ec9bcbcd847a731dc003e
MicMetz <mime9599@colorado.edu>
2023-05-13 06:19:53 -0700

    fix

--- components/Article.js
+++ components/Article.js
@@ -5,16 +5,8 @@ import Link from 'next/link'
 
 export default function ArticleCover ({ article }) {
   return (
-    <div className = "Cover">
-      <Link href = {`/articles/${article.sug}`}>
-        <a >
-          <img src = {article.coverImage} alt = {article.title}/>
-        </a >
-      </Link >
-      <div className = "article-cover__text">
-        <h3 >{article.title}</h3 >
-        <p >{article.excerpt}</p >
-      </div >
+    <div className = "">
+
     </div >
   )
 }

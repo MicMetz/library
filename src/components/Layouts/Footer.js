0a041640c347143a1a2e9ea6038f72e0e92f24d3
MicMetz <mime9599@colorado.edu>
2023-06-09 05:06:33 -0700

    redesign adjustments

--- src/components/Layouts/Footer.js
+++ src/components/Layouts/Footer.js
@@ -6,67 +6,66 @@ import UnderlineHoverLink from "../Links/UnderlineHoverLink.js";
 
 
 export default function Footer() {
-		return (
-				<StyledFooter >
-						<FooterContent >
-								<FooterLinkList >
-										<FooterLinkColumn >
-												<FooterLinkTitle >Email</FooterLinkTitle >
-												<FooterLinkItem href = "mailto:mime9599@colorado.edu" >
-														mime9599@colorado.edu
-												</FooterLinkItem >
-										</FooterLinkColumn >
+  return (
+    <StyledFooter >
+      <FooterContent >
+        <FooterLinkList >
+          <FooterLinkColumn >
+            <FooterLinkTitle >Email</FooterLinkTitle >
+            <FooterLinkItem href = "mailto:mime9599@colorado.edu" >
+              mime9599@colorado.edu
+            </FooterLinkItem >
+          </FooterLinkColumn >
 
-										<FooterLinkColumn >
-												<FooterSocialMediaIcons >
-														<FooterLinkItem href = "https://github.com/MicMetz" >
-																<a rel = "me" href = "https://github.com/MicMetz" >
-																		<GithubIcon />
-																</a >
-														</FooterLinkItem >
-														<FooterLinkItem href = "https://www.linkedin.com/in/michael-metzjer/" >
-																<a rel = "me" href = "https://www.linkedin.com/in/michael-metzjer/" >
-																		<LinkedInIcon />
-																</a >
-														</FooterLinkItem >
-														<FooterLinkItem href = "https://bsky.app/" >
-																<a rel = "me" href = "https://bsky.app/" >
-																		<TwitterIcon />
-																</a >
-														</FooterLinkItem >
-												</FooterSocialMediaIcons >
-										</FooterLinkColumn >
+          <FooterLinkColumn >
+            <FooterSocialMediaIcons >
+              <FooterLinkItem href = "https://github.com/MicMetz" >
+                <a rel = "me" href = "https://github.com/MicMetz" >
+                  <GithubIcon />
+                </a >
+              </FooterLinkItem >
+              <FooterLinkItem href = "https://www.linkedin.com/in/michael-metzjer/" >
+                <a rel = "me" href = "https://www.linkedin.com/in/michael-metzjer/" >
+                  <LinkedInIcon />
+                </a >
+              </FooterLinkItem >
+              <FooterLinkItem href = "https://bsky.app/" >
+                <a rel = "me" href = "https://bsky.app/" >
+                  <TwitterIcon />
+                </a >
+              </FooterLinkItem >
+            </FooterSocialMediaIcons >
+          </FooterLinkColumn >
 
-								</FooterLinkList >
+        </FooterLinkList >
 
-								<FooterSitemapList >
-										{[
-												{ text: "The Garden", slug: "/garden" },
-												{ text: "Essays", slug: "/essays" },
-												{ text: "About", slug: "/about" },
-												{ text: "Notes", slug: "/notes" },
-												{ text: "Topics", slug: "/topics" },
-												{ text: "Now", slug: "/now" },
-												{ text: "Library", slug: "/library" },
-												{ text: "Books", slug: "/books" },
-												{ text: "Changelog", slug: "/changelog" },
-												{ text: "Projects", slug: "/Projects" },
-												{ text: "Colophon", slug: "/colophon" },
-										].map( ( link, i ) => {
-												return (
-														<li key = {i} >
-																<UnderlineHoverLink href = {link.slug} >
-																		{link.text}
-																</UnderlineHoverLink >
-														</li >
-												);
-										} )}
-								</FooterSitemapList >
+        <FooterSitemapList >
+          {[
+            { text: "The Digital Garden", slug: "https://micmetz.github.io/Garden" },
+            { text: "Essays", slug: "https://micmetz.github.io/Essays" },
+            { text: "About", slug: "/About" },
+            { text: "Notes", slug: "https://micmetz.github.io/Notes" },
+            { text: "Topics", slug: "https://micmetz.github.io/topics" },
+            { text: "Now", slug: "https://micmetz.github.io/now" },
+            { text: "Library", slug: "/library" },
+            { text: "Books", slug: "library/books" },
+            { text: "Projects", slug: "https://micmetz.github.io/Projects" },
+            { text: "Changelog", slug: "/changelog" },
+          ].map( ( link, i ) => {
+            return (
+              <li key = {i} >
+                <UnderlineHoverLink href = {link.slug} >
+                  {link.text}
+                </UnderlineHoverLink >
+              </li >
+            );
+          } )}
+        </FooterSitemapList >
 
 
-						</FooterContent >
-				</StyledFooter >
-		);
+      </FooterContent >
+    </StyledFooter >
+  );
 };
 
 
